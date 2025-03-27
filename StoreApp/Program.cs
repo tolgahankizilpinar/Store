095d1ea743ddafc34746ef18431bfc4b2f2d3cde
Your Name <you@example.com>
2025-03-27 19:35:12 +0300

    Repository-addSQLitePackageonCLI

--- StoreApp/Program.cs
+++ StoreApp/Program.cs
@@ -4,6 +4,11 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name:"default", 
+    pattern:"{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
