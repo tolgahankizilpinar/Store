baa8ab463b50e6bb9bc551c7a0530567bc0af167
Your Name <you@example.com>
2025-03-28 13:08:36 +0300

    ConnectionString

--- StoreApp/obj/Debug/net9.0/StoreApp.AssemblyInfo.cs
+++ StoreApp/obj/Debug/net9.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c582427d0b4e92ba5721fd95656b8515498ba57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+095d1ea743ddafc34746ef18431bfc4b2f2d3cde")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
