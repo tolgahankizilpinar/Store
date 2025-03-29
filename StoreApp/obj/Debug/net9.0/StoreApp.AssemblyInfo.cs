cc845868b0b09b0a423159b375ee189b970d0501
Your Name <you@example.com>
2025-03-29 13:19:54 +0300

    Bootstrap

--- StoreApp/obj/Debug/net9.0/StoreApp.AssemblyInfo.cs
+++ StoreApp/obj/Debug/net9.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10b77d86854a7fea966237a40ae0b6291accbfab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+277d680195ebfd5be63ae1bf2558a43950431e00")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
