eea1f6a7a3d1c6d7e9bd6272761dd1ae2eac42c9
Your Name <tolgahan.kizilpinar@hotmail.com>
2025-04-28 00:13:25 +0300

    Custom Tag Helpers

--- Services/obj/Debug/net9.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net9.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aaf75a3e96c9b9357ff6470f2948554fe77415f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58932a85041f3494d5d38ba3a6caaeb9a55fdfc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
